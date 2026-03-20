5d4a167f293be35ff5510f9e1ef8929e464d061d
SteliosSpanos <stylspan19@gmail.com>
2026-03-20 13:58:29 +0200

    Adds ami and policy data sources

--- terraform/data.tf
+++ terraform/data.tf
@@ -3,3 +3,40 @@ data "aws_availability_zones" "available" {
 }
 
 data "aws_caller_identity" "current" {}
+
+data "external" "my_ip" {
+  program = ["bash", "${path.module}/scripts/my_ip_json.sh"]
+}
+
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*-kernel-*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
