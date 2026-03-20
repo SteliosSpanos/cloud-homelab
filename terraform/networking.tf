6cf7b69754562f30ceb8d3c3c9bcddca0484a34e
SteliosSpanos <stylspan19@gmail.com>
2026-03-20 10:27:57 +0200

    Creates vpc and subnets

---
+++ terraform/networking.tf
@@ -0,0 +1,30 @@
+resource "aws_vpc" "homelab_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "homelab_public_subnet" {
+  vpc_id                  = aws_vpc.homelab_vpc.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "${var.project_name}-public-subnet"
+  }
+}
+
+resource "aws_subnet" "homelab_private_subent" {
+  vpc_id            = aws_vpc.homelab_vpc.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "${var.project_name}-private-subnet"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,33 @@
+variable "profile" {
+  type        = string
+  description = "AWS CLI profile"
+}
+
+variable "region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "project_name" {
+  type        = string
+  default     = "cloud-homelab"
+  description = "Project name for resource naming"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  default     = "10.0.0.0/16"
+  description = "VPC CIDR block"
+}
+
+variable "public_subnet_cidr" {
+  type        = string
+  default     = "10.0.1.0/24"
+  description = "Public subnet CIDR block"
+}
+
+variable "private_subnet_cidr" {
+  type        = string
+  default     = "10.0.2.0/24"
+  description = "Private subnet CIDR block"
+}
