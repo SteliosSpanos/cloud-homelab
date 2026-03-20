1215c5aa94c2d3a1d6e8f94890efb12fd3b668d0
SteliosSpanos <stylspan19@gmail.com>
2026-03-20 22:45:47 +0200

    Creates public nacls

---
+++ terraform/nacl.tf
@@ -0,0 +1,139 @@
+/*
+    NACLs inbound and outbound rules
+    for each subnet
+*/
+
+resource "aws_network_acl" "public" {
+  vpc_id     = aws_vpc.homelab_vpc.id
+  subnet_ids = [aws_subnet.homelab_public_subnet.id]
+
+  tags = {
+    Name = "${var.project_name}-public-nacl"
+  }
+}
+
+resource "aws_network_acl" "private" {
+  vpc_id = aws_vpc.homelab_vpc.id
+  subnet_ids = [
+    aws_subnet.homelab_private_subnet_1,
+    aws_subnet.homelab_private_subnet_2
+  ]
+
+  tags = {
+    Name = "${var.project_name}-private-nacl"
+  }
+}
+
+// Inbound Public
+
+resource "aws_network_acl_rule" "public_inbound_ssh" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "${data.external.my_ip.result.ip}/32"
+  from_port      = 22
+  to_port        = 22
+}
+
+resource "aws_network_acl_rule" "public_inbound_http" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 110
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 80
+  to_port        = 80
+}
+
+resource "aws_network_acl_rule" "public_inbound_https" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 120
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 443
+  to_port        = 443
+}
+
+resource "aws_network_acl_rule" "public_inbound_icmp" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 130
+  egress         = false
+  protocol       = "icmp"
+  rule_action    = "allow"
+  cidr_block     = "${data.external.my_ip.result.ip}/32"
+  from_port      = -1
+  to_port        = -1
+}
+
+resource "aws_network_acl_rule" "public_inbound_ephemeral" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 140
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 1024
+  to_port        = 65535
+}
+
+// Outbound Public
+
+resource "aws_network_acl_rule" "public_outbound_http" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 100
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 80
+  to_port        = 80
+}
+
+resource "aws_network_acl_rule" "public_outbound_https" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 110
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 443
+  to_port        = 443
+}
+
+resource "aws_network_acl_rule" "public_outbound_ssh_to_private" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 120
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = aws_subnet.homelab_private_subnet_1.cidr_block
+  from_port      = 22
+  to_port        = 22
+}
+
+resource "aws_network_acl_rule" "public_outbound_icmp" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 130
+  egress         = true
+  protocol       = "icmp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = -1
+  to_port        = -1
+}
+
+resource "aws_network_acl_rule" "public_outbound_ephemeral" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 140
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 1024
+  to_port        = 65535
+}
