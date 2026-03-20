9f9a4f38b617ab2fc500405fbb0444f443cb3c95
SteliosSpanos <stylspan19@gmail.com>
2026-03-20 22:54:09 +0200

    Creates private nacls

--- terraform/nacl.tf
+++ terraform/nacl.tf
@@ -137,3 +137,86 @@ resource "aws_network_acl_rule" "public_outbound_ephemeral" {
   from_port      = 1024
   to_port        = 65535
 }
+
+// Inbound Private
+
+resource "aws_network_acl_rule" "private_inbound_ssh_from_public" {
+  network_acl_id = aws_network_acl.private.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = aws_subnet.homelab_public_subnet.cidr_block
+  from_port      = 22
+  to_port        = 22
+}
+
+resource "aws_network_acl_rule" "private_inbound_icmp" {
+  network_acl_id = aws_network_acl.private.id
+  rule_number    = 110
+  egress         = false
+  protocol       = "icmp"
+  rule_action    = "allow"
+  cidr_block     = var.vpc_cidr
+  from_port      = -1
+  to_port        = -1
+}
+
+resource "aws_network_acl_rule" "private_inbound_ephemeral_from_internet" {
+  network_acl_id = aws_network_acl.private.id
+  rule_number    = 120
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 1024
+  to_port        = 65535
+}
+
+// Outbound Private
+
+resource "aws_network_acl_rule" "private_outbound_http" {
+  network_acl_id = aws_network_acl.private.id
+  rule_number    = 100
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 80
+  to_port        = 80
+}
+
+resource "aws_network_acl_rule" "private_outbound_https" {
+  network_acl_id = aws_network_acl.private.id
+  rule_number    = 110
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 443
+  to_port        = 443
+}
+
+resource "aws_network_acl_rule" "private_outbound_icmp" {
+  network_acl_id = aws_network_acl.private.id
+  rule_number    = 120
+  egress         = true
+  protocol       = "icmp"
+  rule_action    = "allow"
+  cidr_block     = var.vpc_cidr
+  from_port      = -1
+  to_port        = -1
+}
+
+resource "aws_network_acl_rule" "private_outbound_ephemeral" {
+  network_acl_id = aws_network_acl.private.id
+  rule_number    = 130
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = var.vpc_cidr
+  from_port      = 1024
+  to_port        = 65535
+}
+
+
