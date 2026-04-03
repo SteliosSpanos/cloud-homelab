749e0ba463f260fb8e3ce5e14194d1115a9c70dd
SteliosSpanos <stylspan19@gmail.com>
2026-04-03 13:46:57 +0300

    Includes the bucket policy for the backend

--- terraform/data.tf
+++ terraform/data.tf
@@ -367,3 +367,26 @@ data "aws_iam_policy_document" "kms_key_policy" {
     }
   }
 }
+
+// S3 Bucket Policy (Remote Backend)
+
+data "aws_iam_policy_document" "state_force_ssl" {
+  statement {
+    sid    = "AllowSSLOnly"
+    effect = "Deny"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = ["s3:*"]
+    resources = [
+      aws_s3_bucket.terraform_state.arn,
+      "${aws_s3_bucket.terraform_state.arn}/*"
+    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
